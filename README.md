d49123a997338d352ceccb8a37d83a28583496db
Martin Takáč <martin@takac.name>
2015-02-04 19:39:52 +0100

    first commit

--- README.md
+++ README.md
@@ -1,2 +1,74 @@
-# nette-response-spreadsheet
+nette-response-spreadsheet
+==========================
+
 Spreadsheet Nette responses.
+
+Use:
+
+```php
+use Taco\Nette\Application\Responses;
+
+class SomePresenter extends BasePresenter
+{
+    function actionDefault()
+    {
+        $data = [
+            [ 'George', 'age' => 15, 'grade' => 2, ],
+            [ 'Jack', 'age' => 17, 'grade' => 4, ],
+            [ 'name' => 'Mary', 'age' => 17, 'grade' => 1, ],
+        ];
+
+        $response = new Responses\SpreadsheetResponse($data);
+        $this->sendResponse( $response );
+    }
+}
+```
+
+With headers
+
+```php
+use Taco\Nette\Application\Responses;
+
+class SomePresenter extends BasePresenter
+{
+    function actionDefault()
+    {
+        $headers = [ 'Name', 'Age', 'Grade']
+        $data = [
+            [ 'George', 15, 2, ],
+            [ 'Jack', 17, 4, ],
+            [ 'Mary', 17, 1, ],
+        ];
+
+        $response = new Responses\SpreadsheetResponse($data, $headers);
+        $this->sendResponse( $response );
+    }
+}
+```
+
+Individual settings example:
+
+```php
+use Taco\Nette\Application\Responses;
+
+class SomePresenter extends BasePresenter
+{
+    function actionDefault()
+    {
+        $headers = [ 'Name', 'Age', 'Grade']
+        $data = [
+            [ 'George', 15, 2, ],
+            [ 'Jack', 17, 4, ],
+            [ 'Mary', 17, 1, ],
+        ];
+
+        $response = new Responses\SpreadsheetResponse($data, $headers);
+        $response
+            ->setFilename('export')
+            ->setTitle('Export');
+        $this->sendResponse( $response );
+    }
+}
+
+
+```
---
+++ libs/Taco/Nette/Application/Responses/SpreadsheetResponse.php
@@ -0,0 +1,241 @@
+<?php
+/**
+ * This file is part of the Taco Projects.
+ *
+ * Copyright (c) 2004, 2013 Martin Takáč (http://martin.takac.name)
+ *
+ * For the full copyright and license information, please view
+ * the file LICENCE that was distributed with this source code.
+ *
+ * PHP version 5.3
+ *
+ * @author     Martin Takáč (martin@takac.name)
+ */
+
+namespace Taco\Nette\Application\Responses;
+
+
+use Nette,
+	Nette\Image,
+	Nette\Application,
+	Nette\Http\IRequest,
+	Nette\Http\IResponse;
+use Traversable,
+	DateTime;
+
+
+/**
+ * Spreadsheet response
+ */
+class SpreadsheetResponse extends Nette\Object implements Application\IResponse
+{
+
+
+	/**
+	 * Implementace konvertoru do XLS.
+	 * @var XlsProcesor
+	 */
+	private $procesor;
+
+
+	/**
+	 * Jméno souboru posílané současně s exportem,
+	 * @var string
+	 */
+	private $version = Null;
+
+
+	/**
+	 * Jméno souboru posílané současně s exportem,
+	 * @var string
+	 */
+	private $filename;
+
+
+	/**
+	 * @var array
+	 */
+	private $properties = array();
+
+
+	/**
+	 * @var Traversable
+	 */
+	private $headers;
+
+
+	/**
+	 * @var Traversable
+	 */
+	private $data;
+
+
+	/**
+	 * @param Traversable $headers
+	 * @param Traversable $data
+	 * @param XlsProcesor $procesor
+	 */
+	function __construct(Traversable $data, array $headers = array(), XlsProcesor $procesor = Null)
+	{
+		$this->headers = $headers;
+		$this->data = $data;
+		if ($procesor) {
+			$this->procesor = $procesor;
+		}
+	}
+
+
+
+	/**
+	 * Volitelné přiřazení jména souboru.
+	 */
+	function setFilename($s)
+	{
+		$this->filename = (string)$s;
+		return $this;
+	}
+
+
+
+	/**
+	 * @param string
+	 */
+	function setCreator($s)
+	{
+		$this->properties['creator'] = (string)$s;
+		return $this;
+	}
+
+
+
+	/**
+	 * @param string
+	 */
+	function setCreated(DateTime $m)
+	{
+		$this->properties['created'] = $m->format('Y-m-d H:i:s');
+		return $this;
+	}
+
+
+
+	/**
+	 * @param string
+	 */
+	function getModified(DateTime $m)
+	{
+		$this->properties['modified'] = $m->format('Y-m-d H:i:s');
+		return $this;
+	}
+
+
+
+	/**
+	 * @param string
+	 */
+	function setLastModifiedBy($s)
+	{
+		$this->properties['lastModifiedBy'] = (string)$s;
+		return $this;
+	}
+
+
+
+	/**
+	 * @param string
+	 */
+	function setTitle($s)
+	{
+		$this->properties['title'] = (string)$s;
+		return $this;
+	}
+
+
+
+	/**
+	 * @param string
+	 */
+	function setSubject($s)
+	{
+		$this->properties['subject'] = (string)$s;
+		return $this;
+	}
+
+
+
+	/**
+	 * @param string
+	 */
+	function setDescription($s)
+	{
+		$this->properties['description'] = (string)$s;
+		return $this;
+	}
+
+
+
+	/**
+	 * @param string
+	 */
+	function setKeywords($s)
+	{
+		$this->properties['keywords'] = (string)$s;
+		return $this;
+	}
+
+
+
+	/**
+	 * @param string
+	 */
+	function setCategory($s)
+	{
+		$this->properties['category'] = (string)$s;
+		return $this;
+	}
+
+
+
+	/**
+	 * @param Nette\Http\IRequest
+	 * @param Nette\Http\IResponse
+	 */
+	function send(IRequest $httpRequest, IResponse $httpResponse)
+	{
+		$httpResponse->setContentType($this->getProcesor()->getContentType());
+		$httpResponse->setHeader('Cache-Control', 'max-age=0');
+		$httpResponse->setHeader('Content-Transfer-Encoding', 'binary');
+
+		if ($this->filename) {
+			$httpResponse->setHeader('Content-Disposition', 'attachment;filename="' . $this->filename . '.' . $this->getProcesor()->getExtension() . '"');
+		}
+
+		$this->getProcesor()
+				->setProperties($this->properties)
+				->setHeaders($this->headers)
+				->echo_($this->data);
+	}
+
+
+
+	function test()
+	{
+		$this->getProcesor()->echo_($this->data);
+	}
+
+
+
+	// -- PRIVATE ------------------------------------------------------
+
+
+
+	private function getProcesor()
+	{
+		if (empty($this->procesor)) {
+			$this->procesor = new SpreadsheetProcesor($this->version);
+		}
+		return $this->procesor;
+	}
+
+
+}
